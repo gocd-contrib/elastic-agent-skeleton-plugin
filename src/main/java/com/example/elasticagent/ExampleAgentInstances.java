10a879ab9b118df972e2e12777aacd002079ce70
Benjamin Alexander <benalexander71792@gmail.com>
2018-12-14 20:51:38 -0800

    WIP: hacking together create instance

--- src/main/java/com/example/elasticagent/ExampleAgentInstances.java
+++ src/main/java/com/example/elasticagent/ExampleAgentInstances.java
@@ -23,10 +23,12 @@ import com.example.elasticagent.requests.CreateAgentRequest;
 import com.thoughtworks.go.plugin.api.logging.Logger;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.InstanceType;
+import software.amazon.awssdk.services.ec2.model.RunInstancesMonitoringEnabled;
 import software.amazon.awssdk.services.ec2.model.RunInstancesRequest;
 import software.amazon.awssdk.services.ec2.model.RunInstancesResponse;
 import software.amazon.awssdk.services.ec2.model.Tag;
 import software.amazon.awssdk.services.ec2.model.CreateTagsRequest;
+import software.amazon.awssdk.services.ec2.model.Instance;
 
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
@@ -40,41 +42,55 @@ public class ExampleAgentInstances implements AgentInstances<ExampleInstance> {
     private boolean refreshed;
     public Clock clock = Clock.DEFAULT;
 
+    private void addNewline(String str, String newLine)
+    {
+    	str = str + "\n" + newLine; 
+    }
+    
     @Override
     public ExampleInstance create(CreateAgentRequest request, PluginSettings settings) throws Exception {
         // TODO: Implement me!
     	LOG.info("MyPlugin: create");
-    	Ec2Client myClient = Ec2Client.create();
+    	Ec2Client ec2 = Ec2Client.create();
     	
+    	String userData = "";
+    	addNewline(userData, "<powershell>");
+    	addNewline(userData, "mkdir \"C:\\Program Files (x86)\\Go Agent\\config\"");
+    	addNewline(userData, "$key = \"" + request.autoRegisterKey() + "\"");
+    	addNewline(userData, "$resources = \"Windows,EC2\""); //TODO: figure out how to tag resources properly
+    	addNewline(userData, "$UserInfoToFile = @\"");
+    	addNewline(userData, "agent.auto.register.key=$key");
+    	addNewline(userData, "agent.auto.register.resources=$resources");
+    	addNewline(userData, "\"@");
+    	addNewline(userData, "$UserInfoToFile | Out-File -FilePath \"C:\\Program Files (x86)\\Go Agent\\config\\autoregister.properties\" -Encoding ASCII");
+    	addNewline(userData, "Invoke-WebRequest -OutFile C:\\Users\\Administrator\\Downloads\\go-agent-18.11.0-8024-jre-64bit-setup.exe https://download.gocd.org/binaries/18.11.0-8024/win/go-agent-18.11.0-8024-jre-64bit-setup.exe");
+    	addNewline(userData, "C:\\Users\\Administrator\\Downloads\\go-agent-18.11.0-8024-jre-64bit-setup.exe /S /START_AGENT=YES /SERVERURL=`\"" + settings.getGoServerUrl() + "`\"");  //TODO: set server URL correctly
+    	addNewline(userData, "</powershell>");	
     	
     	RunInstancesRequest runInstancesRequest = RunInstancesRequest.builder()
     			.imageId("ami-017bf00eb0d4c7182")
     			.instanceType(InstanceType.T2_MICRO)
+    			.securityGroupIds("sg-00a22b0befc186b4c")
+    			.keyName("MyFirstKey.pem")
+    			.monitoring(RunInstancesMonitoringEnabled.builder().enabled(true).build())
+    			.userData(userData)
     			.minCount(1)
-    			.maxCount(1).build();
-    			
-
+    			.maxCount(1)
+    			.build();
+    	
+    	RunInstancesResponse response = ec2.runInstances(runInstancesRequest);		
+    	LOG.info("MyPlugin: create: created " + response.instances().size() + " instances");
+    	for (Instance instance : response.instances())
+    	{
+    		LOG.info("MyPlugin: create: id: " + instance.instanceId());
+    	}
+    	
+    	Instance newinstance = response.instances().get(0);
+    	
+    	ExampleInstance newInstance = new ExampleInstance(newinstance.imageId(), clock.now().toDate(), request.properties(), request.environment(), request.jobIdentifier());  //TODO: tag ec2 instance to match this
+    	//TODO: add instances to this thing ConcurrentHashMap<String, ExampleInstance> instances = new ConcurrentHashMap<>();
+    	return newInstance;
     			/*
-    		    newInstances = ec2.create_instances(LaunchTemplate={'LaunchTemplateName': 'BulletHellMetaServer'},
-                KeyName='MyFirstKey.pem',
-                SecurityGroupIds=[
-                    'sg-00a22b0befc186b4c',
-                ],
-                UserData=
-                """<powershell>
-mkdir "C:\\Program Files (x86)\\Go Agent\\config"
-$key = "4f6482d7-9a7c-4ced-9b4d-694ee9f345c2"
-$resources = "Windows,EC2"
-$UserInfoToFile = @"
-agent.auto.register.key=$key
-agent.auto.register.resources=$resources
-"@
-$UserInfoToFile | Out-File -FilePath "C:\\Program Files (x86)\\Go Agent\\config\\autoregister.properties" -Encoding ASCII
-Invoke-WebRequest -OutFile C:\\Users\\Administrator\\Downloads\\go-agent-18.11.0-8024-jre-64bit-setup.exe https://download.gocd.org/binaries/18.11.0-8024/win/go-agent-18.11.0-8024-jre-64bit-setup.exe
-C:\\Users\\Administrator\\Downloads\\go-agent-18.11.0-8024-jre-64bit-setup.exe /S /START_AGENT=YES /SERVERURL=`"https://gocd.benalexander.org:8154/go`"
-</powershell>""",
-                InstanceType='t2.micro',
-                Monitoring={'Enabled':True},
                 TagSpecifications=[{
                     'ResourceType':'instance',
                     'Tags': [{
@@ -84,7 +100,7 @@ C:\\Users\\Administrator\\Downloads\\go-agent-18.11.0-8024-jre-64bit-setup.exe /
                 }])*/
     	
     	
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
 //        ExampleInstance instance = ExampleInstance.create(request, settings);
 //        register(instance);
 //        return instance;
@@ -109,7 +125,7 @@ C:\\Users\\Administrator\\Downloads\\go-agent-18.11.0-8024-jre-64bit-setup.exe /
     public void terminateUnregisteredInstances(PluginSettings settings, Agents agents) throws Exception {
         // TODO: Implement me!
     	LOG.info("MyPlugin: terminateUnregisteredInstances");
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
 
 //        ExampleAgentInstances toTerminate = unregisteredAfterTimeout(settings, agents);
 //        if (toTerminate.instances.isEmpty()) {
